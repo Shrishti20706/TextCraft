407fed63842f0b83e23974c4bde397a121ead525
Shrishti20706 <shrishtijain20706@acropolis.in>
2023-10-22 16:52:37 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 export default function Navbar(props) {
@@ -8,21 +8,21 @@ export default function Navbar(props) {
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode === 'dark' ? 'dark' : 'light'}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">{props.title}</Link>
+          <NavLink className="navbar-brand" to="/">{props.title}</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-a  text-decoration-none text-dark mx-3" aria-current="page" to="/TextUtils">Home</Link>
+                <NavLink className="nav-a  text-decoration-none text-dark mx-3" aria-current="page" to="/TextUtils">Home</NavLink>
 
                 {/* <a className="nav-a  text-decoration-none text-dark" aria-current="page" href="#">Home</a> */}
               </li>
 
 
               <li className="nav-item">
-                <Link className="nav-a a text-decoration-none text-dark mx-3" to="/about">{props.aboutText}</Link>
+                <NavLink className="nav-a a text-decoration-none text-dark mx-3" to="/about">{props.aboutText}</NavLink>
               </li>
 
 
